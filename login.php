2dbd5027a2a253604af54f868d886b17f5f1b7b3
Nikitka228pro1488 <isayev.nikitos@gmail.com>
2017-06-08 01:41:23 +0700

    Update login.php

--- login.php
+++ login.php
@@ -8,21 +8,26 @@
                   <h2 class="post_ttl2">АВТОРИЗАЦИЯ</h2>
                   <div id="basket" style="background:none;">
                      <?php
-			$login     = htmlspecialchars($_POST['login']);
-			$password  = htmlspecialchars($_POST['password']);
-			$logins    = file('files/logins_Op9nHqy72gT3bFq7sv.txt', FILE_IGNORE_NEW_LINES);
-			$passwords = file('files/passwords_M5tYgqBa6v2aw9Ij1.txt', FILE_IGNORE_NEW_LINES);
-			for ($i = 0; $i < count($logins); $i++) {
-			    if ($login == $logins[$i] && $password == $passwords[$i]) {
-				$_SESSION['login'] = $login;
-				if ($_SESSION['login'] == 'isaev' && $password == 'nikitos') {
-				    $_SESSION['adminmode'] = 'activated';
-				}
-				echo '<script>location.href="', $_SERVER['HTTP_REFERER'], '"</script>';
-				exit;
-			    }
+			if (isset($_SESSION['login'])) {
+				echo '<script>location.href="index.php"</script>';
 			}
-			echo '<p align="center" style="font-size:28px;color:#008000;"><b>Ошибка ввода, Попробуйте снова</b></p>';
+			else {  
+				$login     = htmlspecialchars($_POST['login']);
+				$password  = htmlspecialchars($_POST['password']);
+				$logins    = file('files/logins_Op9nHqy72gT3bFq7sv.txt', FILE_IGNORE_NEW_LINES);
+				$passwords = file('files/passwords_M5tYgqBa6v2aw9Ij1.txt', FILE_IGNORE_NEW_LINES);
+				for ($i = 0; $i < count($logins); $i++) {
+				    if ($login == $logins[$i] && $password == $passwords[$i]) {
+					$_SESSION['login'] = $login;
+					if ($_SESSION['login'] == 'isaev' && $password == 'nikitos') {
+					    $_SESSION['adminmode'] = 'activated';
+					}
+					echo '<script>location.href="', $_SERVER['HTTP_REFERER'], '"</script>';
+					exit;
+				    }
+				}
+				echo '<p align="center" style="font-size:28px;color:#008000;"><b>Ошибка ввода, Попробуйте снова</b></p>';
+			}
 			?>
                   </div>
                </div>
